c70090f5a04564d34cefd706999d1b6ba6f735e1
Yoan Bonnefis <yoan.bonnefis@yahoo.fr>
2017-10-25 08:54:30 +0200

    exo5

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+/*var nombreUn = document.form1.premier_nombre.value;
+var nombreDeux = document.form1.deuxieme_nombre.value;*/
+//var multiply =  nombreUn * nombreDeux;
+function multiply() {
+  var nombreUn = document.form1.premier_nombre.value;
+  var nombreDeux = document.form1.deuxieme_nombre.value;
+ alert(nombreUn * nombreDeux);
+}
